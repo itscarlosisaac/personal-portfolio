8955225b4de8794b563ff4babb9221d94b736bdd
Carlos Martinez <itscarlosisaac@gmail.com>
2024-03-07 16:01:42 -0500

    Removed extra social in homepage.

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -5,7 +5,6 @@ import HeroBanner from "../components/HeroBanner/HeroBanner.tsx";
 import Contact from "../components/Contact/Contact.tsx";
 import MascotContainer from "../components/Mascot/MascotContainer.tsx";
 import NavigatorHandle from "../components/NavigatorHandle/NavigatorHandle.tsx";
-import SocialMenu from "../components/SocialMenu/SocialMenu.tsx";
 import AboutSection from "../components/About/AboutSection.tsx";
 
 function HomeView() {
@@ -14,7 +13,6 @@ function HomeView() {
 		<Grid w={"100vw"} gridTemplateColumns={{base: "1fr" }} position={"relative"}>
 			<NavigationContainer />
 			<NavigatorHandle />
-			<SocialMenu />
 			<VStack >
 				<HeroBanner />
 				<AboutSection />
