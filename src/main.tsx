c39c638af3f6676fa18f0b5f6737525245790452
Carlos Martinez <itscarlosisaac@gmail.com>
2024-03-09 16:15:54 -0500

    Added vercel analytics.

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,13 @@ import App from './App.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from "./theme";
 import ReactGA from 'react-ga4';
+import { Analytics } from "@vercel/analytics/react"
 
 ReactGA.initialize('G-FGETTJ0DJZ');
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
       <ChakraProvider theme={theme}>
+        <Analytics/>
         <App />
       </ChakraProvider>
   </React.StrictMode>,
