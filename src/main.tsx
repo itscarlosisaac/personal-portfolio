ac907be27925d22c1cf8e1035669c28d93e75799
Carlos Martinez <itscarlosisaac@gmail.com>
2024-03-09 22:12:37 -0500

    Added Vercel Speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import { ChakraProvider } from '@chakra-ui/react'
 import theme from "./theme";
 import ReactGA from 'react-ga4';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactGA.initialize('G-FGETTJ0DJZ');
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
       <ChakraProvider theme={theme}>
         <Analytics/>
+        <SpeedInsights/>
         <App />
       </ChakraProvider>
   </React.StrictMode>,
